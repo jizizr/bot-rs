8f29fcdceb1cc382b46db6d6d0bfdc137a780342
Allen Jeo <jizizr1234@gmail.com>
2023-08-09 20:10:47 +0800

    增加二维码功能，优化处理逻辑

--- src/funcs/command/short.rs
+++ src/funcs/command/short.rs
@@ -1,6 +1,10 @@
 use super::*;
+use fast_qr::convert::{image::ImageBuilder, Builder, Shape};
+use fast_qr::qr::QRBuilder;
 use regex::Regex;
 use serde_json::json;
+use teloxide::types::InputFile;
+
 lazy_static! {
     static ref USAGE: String = ShortCmd::command().render_help().to_string();
     static ref MATCH: Regex = Regex::new(r#"(\s|^|https?://)([^:\./\s]+\.)+[^\d\./:\s\\"]{2,}(:(\d{1,4}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?(/\S*)*(\s|$)"#).unwrap();
@@ -18,10 +22,10 @@ error_fmt!(USAGE);
     disable_help_flag = true
 )]
 struct ShortCmd {
-    ///短链后缀
-    surl: Option<String>,
     ///长链接
     url: Option<String>,
+    ///短链后缀
+    surl: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -40,20 +44,29 @@ fn fix_start(u: String) -> String {
 
 async fn get_short(msg: &Message) -> Result<String, AppError> {
     let short = ShortCmd::try_parse_from(getor(msg).unwrap().split_whitespace())?;
-    let mut url = short
-        .url
-        .or_else(|| {
-            msg.reply_to_message()
-                .map(|m| getor(m).unwrap().to_string())
-        })
-        .ok_or(AppError::CustomError("用法详解：".to_string()))?;
     let request_body;
+    let surl;
+    let mut url: String = match (short.surl, msg.reply_to_message(), &short.url) {
+        (Some(s), _, _) => {
+            surl = Some(s);
+            short.url.unwrap()
+        }
+        (_, Some(reply), _) => {
+            surl = short.url;
+            getor(reply).unwrap().to_string()
+        }
+        (_, _, Some(url_value)) => {
+            surl = None;
+            url_value.to_string()
+        }
+        _ => return Err(AppError::CustomError("用法详解：".to_string())),
+    };
     url = MATCH
         .find(&url)
         .ok_or_else(|| AppError::CustomError("匹配不到符合规则的URL".to_string()))?
         .as_str()
         .to_string();
-    match short.surl {
+    match surl {
         None => request_body = json!({"url":fix_start(url)}),
         Some(s) => request_body = json!({"url":fix_start(url),"shorturl":s}),
     }
@@ -65,7 +78,7 @@ async fn get_short(msg: &Message) -> Result<String, AppError> {
         .json()
         .await?;
     if post_result.code == 200.to_string() {
-        return Ok(format!("短链接：https://774.gs/{}", post_result.shorturl));
+        return Ok(format!("https://774.gs/{}", post_result.shorturl));
     } else if post_result.code == 2003.to_string() {
         return Err(AppError::CustomError("指定的短域已被占用".to_string()));
     } else {
@@ -73,10 +86,32 @@ async fn get_short(msg: &Message) -> Result<String, AppError> {
     }
 }
 
+fn url2qr(url: &str) -> Vec<u8> {
+    let qrcode = QRBuilder::new(url).build().unwrap();
+
+    ImageBuilder::default()
+        .shape(Shape::RoundedSquare)
+        .background_color([255, 255, 255, 0]) // Handles transparency
+        .fit_width(150)
+        .to_pixmap(&qrcode)
+        .encode_png()
+        .unwrap()
+}
+
 pub async fn short(bot: Bot, msg: Message) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let text = get_short(&msg).await.unwrap_or_else(|e| format!("{e}"));
-    bot.send_message(msg.chat.id, text)
-        .reply_to_message_id(msg.id)
-        .await?;
+    match get_short(&msg).await {
+        Ok(url) => {
+            bot.send_photo(msg.chat.id, InputFile::memory(url2qr(&url)))
+                .caption(format!("短链接：{}", url))
+                .send()
+                .await?
+        }
+        Err(e) => {
+            bot.send_message(msg.chat.id, format!("{e}"))
+                .reply_to_message_id(msg.id)
+                .send()
+                .await?
+        }
+    };
     Ok(())
 }
