be83742a408b577432c9515842d8885fe173906c
Allen Joe <jizizr1234@gmail.com>
2024-03-30 17:17:16 +0800

    Remove unnecessary file from .gitignore

---
+++ src/funcs/command/test.rs
@@ -0,0 +1,9 @@
+use super::*;
+
+pub async fn test(bot: Bot, _msg: Message) -> BotResult {
+    let photo = bot.get_user_profile_photos(UserId(1071410342)).await?;
+    let file_id = &photo.photos[0][0].file.id;
+    let f = bot.get_file(file_id).await?;
+    println!("{}", f.path);
+    Ok(())
+}
