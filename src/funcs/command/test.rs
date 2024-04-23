44b0547176c9363200a4839bcb04361b72f98302
Allen Joe <jizizr1234@gmail.com>
2024-04-23 14:56:35 +0800

    (chore) 去除test函数dead_code警告

--- src/funcs/command/test.rs
+++ src/funcs/command/test.rs
@@ -1,10 +1,11 @@
 use super::*;
 
+#[allow(dead_code)]
 pub async fn test(bot: Bot, _msg: Message) -> BotResult {
     let photo = bot.get_user_profile_photos(UserId(1071410342)).await?;
     let file_id = &photo.photos[0][0].file.id;
     let f = bot.get_file(file_id).await?;
-    
+
     println!("{}", f.path);
     Ok(())
 }
