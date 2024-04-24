264928f795563e0f5738cceb92111c9ed764aeab
Allen Joe <jizizr1234@gmail.com>
2024-04-24 15:08:26 +0800

    (chore) 删除多余代码

--- src/funcs/command/test.rs
+++ src/funcs/command/test.rs
@@ -1,11 +1,7 @@
 use super::*;
 
-#[allow(dead_code)]
-pub async fn test(bot: Bot, _msg: Message) -> BotResult {
-    let photo = bot.get_user_profile_photos(UserId(1071410342)).await?;
-    let file_id = &photo.photos[0][0].file.id;
-    let f = bot.get_file(file_id).await?;
-
-    println!("{}", f.path);
+pub async fn test(_bot: Bot, _msg: Message) -> BotResult {
+    #[cfg(debug_assertions)]
+    {}
     Ok(())
 }
